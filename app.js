7fcd81a9218d85f191d57564bcf1af509cc9821f
dermio <thisdjay@gmail.com>
2018-01-25 22:35:46 -0800

    Demo to get D3 chart to appear in Lity lightbox
    Demo to get the D3 chart to appear in the Lity lightbox.
    There are two methods to make the D3 chart appear in a lightbox:
    
    1 An outer <div class="lity-hide"> will have a child
    <div class="chart-container">. The D3 functions will draw the chart
    inside div.chart-container. The functions drawChart() and
    resizeChart() are already called.
    
    2. Comment out the functions drawChart() and resizeChart()
    in d3-chart.js. Next use the Lity event handler:
    $(document).on('lity:open', function(event, instance) { ... });
    
    Inside the event handler for 'lity:open' call drawChart().
    Can also create another event handler for 'lity:resize' to
    call resizeChart().
    
    Problems to fix:
    Neither method is perfect. The D3 chart is no longer responsive
    on window resize.

---
+++ app.js
@@ -0,0 +1,30 @@
+/* Lity demo */
+
+function startApp() {
+  //var instance = lity('https://www.youtube.com/watch?v=XSGBVzeBUbk');
+}
+
+//$(startApp);
+
+
+/*
+$(document).on("lity:open", function (event, instance) {
+  console.log(event);
+  console.log("hello world");
+  //drawChart();
+})*/
+
+
+
+/*
+This code using Lity, to display something that's already on page
+inside a Lightbox
+
+Display the D3 chart inside this <div>, keep it hidden, then use 
+data-lity inline href to load the chart inside the lightbox.
+
+<div id="inline" style="background:#fff" class="lity-hide">
+    Inline content
+</div>
+
+*/
